86c10c086ac37e16d6d29f8bdd66b13363d3e6e5
bill <brkozak@gmail.com>
2019-06-27 21:40:10 -0600

    Print some syscalls in human readable format
    Implemented printing of some syscalls in a human readable format.
    
    At exit of the syscall (note that now since nothing is printed at
    syscall enter, syscalls which never exit will not be displayed)
    we now print out a human readable representation of the syscall for
    some specific calls.
    
    This has been done to demonstrate that we can view the system calls,
    and the arguments to the system calls and also that we can directly
    read the memory of traced process (note that currently we will actually
    segfault if we attempt to read memory that we are not allowed to access
    for any reason and that if the target program uses a clone call to
    implement forking we will attach to the child process but will not
    be able to read its memory).

--- src/c/pseudo-strace.c
+++ src/c/pseudo-strace.c
@@ -16,6 +16,7 @@
 * You should have received a copy of the GNU Lesser General Public License    *
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.       *
 ******************************************************************************/
+#define _GNU_SOURCE
 /******************************************************************************
 *                                  INCLUDES                                   *
 ******************************************************************************/
@@ -25,14 +26,255 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/user.h>
+#include <sys/syscall.h>
+#include <stdint.h>
+#include <assert.h>
+#include <ctype.h>
+#include <string.h>
+/******************************************************************************
+*                                   MACROS                                    *
+******************************************************************************/
+#define SYCALL_ARG(type, n, regs) (type)syscall_arg(n, regs)
+#define SYCALL_RETVAL(type, regs) (type)syscall_retval(regs)
+
+#define SYSCALL_BUF(str, strlen, n, x, regs) \
+	sprint_buffer( \
+		SYCALL_ARG(char*, n, regs), \
+		str, \
+		SYCALL_ARG(ssize_t, x, regs), \
+		strlen \
+	)
+
+#define CHAR_ARR_STRLEN(s) (sizeof(s) - 1)
+/******************************************************************************
+*                                  CONSTANTS                                  *
+******************************************************************************/
+static const ssize_t PRINT_BUFFER_SIZE = 256;
 /******************************************************************************
 *                            FUNCTION DECLARATIONS                            *
 ******************************************************************************/
 static void* init(void *arg);
 static void* handle(void *argg, const struct tracee_state *state);
+static void print_syscall(
+	FILE *fp, pid_t pid, const struct user_regs_struct *regs
+);
+static uint64_t syscall_retval(const struct user_regs_struct *regs);
+static uint64_t syscall_arg(int n, const struct user_regs_struct *regs);
+static int repr_byte(char *str, char byte, ssize_t *space_size);
+static char octal_char(int val, int n);
+static char *sprint_buffer(
+	const char *buffer,
+	char *space,
+	ssize_t buffer_size,
+	ssize_t space_size
+);
 /******************************************************************************
 *                              STATIC FUNCTIONS                               *
 ******************************************************************************/
+static char octal_char(int val, int n)
+{
+	return ((val >> (3 * n)) & 0x7) + '0';
+}
+/*****************************************************************************/
+static int repr_byte(char *str, char byte, ssize_t *space_size)
+{
+	if((byte == '"') || (byte == '\\')) {
+		if(*space_size < 2) {
+			return 0;
+		} else {
+			str[0] = '\\';
+			str[1] = byte;
+			*space_size -= 2;
+			return 2;
+		}
+	} else if(byte == '\n') {
+		if(*space_size < 2) {
+			return 0;
+		} else {
+			str[0] = '\\';
+			str[1] = 'n';
+			*space_size -= 2;
+			return 2;
+		}
+	} else if(isprint(byte) || (byte == '\t')) {
+		if(*space_size == 0) {
+			return 0;
+		} else {
+			str[0] = byte;
+			*space_size -= 1;
+			return 1;
+		}
+	} else {
+		if(*space_size < 4) {
+			return 0;
+		} else {
+			str[0] = '\\';
+			str[1] = octal_char(byte, 2);
+			str[2] = octal_char(byte, 1);
+			str[3] = octal_char(byte, 0);
+			*space_size -= 4;
+			return 4;
+		}
+	}
+}
+/*****************************************************************************/
+static char *sprint_buffer(
+	const char *buffer,
+	char *str,
+	ssize_t buffer_size,
+	ssize_t space_size
+) {
+	int len = 0;
+	char border = '"';
+	const char continuation[] = "\"...";
+
+	space_size -= sizeof(border) + CHAR_ARR_STRLEN(continuation) + 1;
+
+	if(space_size < 0) {
+		return NULL;
+	}
+
+	str[len] = border;
+	len += 1;
+
+	for(size_t i = 0; i < buffer_size; i++) {
+		int s = 0;
+		char c = buffer[i];
+
+		if((s = repr_byte(str + len, c, &space_size)) == 0) {
+			memcpy(str + len, continuation, sizeof(continuation));
+			return str;
+		}
+		len += s;
+	}
+
+	str[len] = border;
+	str[len + 1] = '\0';
+
+	return str;
+}
+/*****************************************************************************/
+static uint64_t syscall_retval(const struct user_regs_struct *regs)
+{
+	return regs->rax;
+}
+/*****************************************************************************/
+static uint64_t syscall_arg(int n, const struct user_regs_struct *regs)
+{
+	assert(n < 6);
+
+	switch(n) {
+	case 0:
+		return regs->rdi;
+	case 1:
+		return regs->rsi;
+	case 2:
+		return regs->rdx;
+	case 3:
+		return regs->r10;
+	case 4:
+		return regs->r8;
+	case 5:
+		return regs->r9;
+	default:
+		return 0;
+	}
+}
+/*****************************************************************************/
+static void print_syscall(
+	FILE *fp, pid_t pid, const struct user_regs_struct *regs
+) {
+	char p_buffer[PRINT_BUFFER_SIZE];
+	int syscall_no = regs->orig_rax;
+
+	switch(syscall_no) {
+	case SYS_write:
+		fprintf(
+			fp, "[ID %d]: write(%d, %s, %ld) = %d\n",
+			pid,
+			SYCALL_ARG(int,     0, regs),
+			SYSCALL_BUF(p_buffer, PRINT_BUFFER_SIZE, 1, 2, regs),
+			SYCALL_ARG(int64_t, 2, regs),
+			SYCALL_RETVAL(int, regs)
+		);
+		break;
+	case SYS_close:
+		fprintf(
+			fp, "[ID %d]: close(%d) = %d\n",
+			pid,
+			SYCALL_ARG(int,     0, regs),
+			SYCALL_RETVAL(int, regs)
+		);
+		break;
+	case SYS_fstat:
+		fprintf(
+			fp, "[ID %d]: fstat(%d, %p) = %d\n",
+			pid,
+			SYCALL_ARG(int,     0, regs),
+			SYCALL_ARG(void*,   1, regs),
+			SYCALL_RETVAL(int, regs)
+		);
+		break;
+	case SYS_mmap:
+		fprintf(
+			fp, "[ID %d]: mmap(%p, %ld, %d, %d, %d, %lu) = %p\n",
+			pid,
+			SYCALL_ARG(void*,    0, regs),
+			SYCALL_ARG(int64_t,  1, regs),
+			SYCALL_ARG(int,      2, regs),
+			SYCALL_ARG(int,      3, regs),
+			SYCALL_ARG(int,      4, regs),
+			SYCALL_ARG(uint64_t, 5, regs),
+			SYCALL_RETVAL(void*,    regs)
+		);
+		break;
+	case SYS_ioctl:
+		fprintf(
+			fp, "[ID %d]: ioctl(%d, %lu, %p) = %d\n",
+			pid,
+			SYCALL_ARG(int,       0, regs),
+			SYCALL_ARG(uint64_t,  1, regs),
+			SYCALL_ARG(void*,     2, regs),
+			SYCALL_RETVAL(int,    regs)
+		);
+		break;
+	case SYS_getpid:
+		fprintf(
+			fp, "[ID %d]: getpid() = %d\n",
+			pid,
+			SYCALL_RETVAL(int,    regs)
+		);
+		break;
+	case SYS_getdents:
+		fprintf(
+			fp, "[ID %d]: getdents(%d, %p, %d) = %d\n",
+			pid,
+			SYCALL_ARG(int,       0, regs),
+			SYCALL_ARG(void*,     1, regs),
+			SYCALL_ARG(int,       2, regs),
+			SYCALL_RETVAL(int,    regs)
+		);
+		break;
+	case SYS_openat:
+		fprintf(
+			fp, "[ID %d]: openat(%d, %p, %d, %d) = %d\n",
+			pid,
+			SYCALL_ARG(int,       0, regs),
+			SYCALL_ARG(void*,     1, regs),
+			SYCALL_ARG(int,       2, regs),
+			SYCALL_ARG(int,       3, regs),
+			SYCALL_RETVAL(int,    regs)
+		);
+		break;
+	default:
+		fprintf(
+			fp, "[ID %d]: syscall(%d, ...) = %lu\n",
+			pid, syscall_no, SYCALL_RETVAL(uint64_t, regs)
+		);
+	}
+}
+/*****************************************************************************/
 static void* init(void *arg)
 {
 	return fopen("/dev/stderr", "w");
@@ -41,19 +283,9 @@ static void* init(void *arg)
 static void* handle(void *arg, const struct tracee_state *state)
 {
 	if(state->status == SYSCALL_ENTER_STOP) {
-		fprintf(
-			arg,
-			"[ID %d]: Enter syscall: %llu\n",
-			state->pid,
-			state->data.regs.orig_rax
-		);
+
 	} else if(state->status == SYSCALL_EXIT_STOP) {
-		fprintf(
-			arg,
-			"[ID %d]: Exit syscall: %llu\n",
-			state->pid,
-			state->data.regs.orig_rax
-		);
+		print_syscall(arg, state->pid, &state->data.regs);
 	} else if(state->status == EXITED_NORMAL) {
 		fprintf(
 			arg,
